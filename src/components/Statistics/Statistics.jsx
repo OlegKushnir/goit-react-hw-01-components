3c6d41e714342d810e5056295b5404a17fc6dd21
OlegKushnir <kushnir.oleg87@gmail.com>
2022-10-25 21:52:25 +0300

    update

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,7 +4,7 @@ import { StatisticsItem } from './StatisticsItem';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      {title ? <h2 className={css.title}>{title}</h2> : ''}
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         {stats.map(({ id, label, percentage }) => (
           <StatisticsItem key={id} label={label} percentage={percentage} />
